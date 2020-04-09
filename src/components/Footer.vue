1697387260873f97f71685562170b413d7929df4
louisaoge <louisaoge@gmail.com>
2020-04-09 11:39:46 +0100

    first commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+    <footer id="custom-footer"> 
+        <span class="mr-4">&copy; BraveTech </span>
+        <span class="mr-4">Private  </span>
+        <span> Terms of Service </span>
+
+    </footer>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,43 @@
+  <template>
+  <header>
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top custom-bg-dark">
+    <a class="navbar-brand" href="#">
+            <img style="max-height:75px;" alt="Vue logo" src="../assets/brave_logo.jpg">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Tasks</a>
+        </li>
+         <li class="nav-item">
+          <a class="nav-link" href="#">Register</a>
+        </li>
+         <li class="nav-item">
+          <a class="nav-link" href="#">Login</a>
+        </li>
+         <li class="nav-item">
+          <a class="nav-link" href="#">Logout</a>
+        </li>
+         <li class="nav-item">
+          <a class="nav-link" href="#">Louisa</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</header>
+    
+</template>
+
+
+  
+<style scoped>
+
+
+</style>
+
